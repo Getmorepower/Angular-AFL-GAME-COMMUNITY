a98a6771805d32ed7d2cc62519c3483801c46adb
Getmorepower <77925240+Getmorepower@users.noreply.github.com>
2021-10-09 01:56:33 +1100

    Delete src directory

--- src/app/ladder.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Ladder } from './ladder';
-
-describe('Ladder', () => {
-  it('should create an instance', () => {
-    expect(new Ladder()).toBeTruthy();
-  });
-});
--- src/app/team.ts
+++
@@ -1,9 +0,0 @@
-export class Team {
-    constructor(
-		public id:number = 0,
-		public name:string = '', 
-		public logo:string = '',
-        public abbrev:string = ''
-	){}
-
-}
